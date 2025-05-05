e92d54e28a2b0c54818ea6f045cbe5181328e6de
10Oliver <oliver.erazo1@gmail.com>
2025-05-05 03:35:07 +0000

    Improve landing design

--- resources/views/costumer/landing.blade.php
+++ resources/views/costumer/landing.blade.php
@@ -20,7 +20,94 @@
         </div>
     </div>
     <div class="py-10 w-4/5 flex flex-col items-center mx-[10%]">
-        <h3 id="ver-cupones" class="text-4xl font-bold">Cupones disponibles</h3>
+        <h3 id="ver-cupones" class="text-4xl font-bold mb-5">Cupones disponibles</h3>
+        <div class="grid grid-cols-3 gap-5">
+            <div class="rounded-xl grid grid-cols-[auto_1fr] border-[#929191] border-[1px] overflow-hidden">
+                <div class="bg-[#a83131] w-[130px] flex justify-center items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-12" viewBox="0 0 24 24">
+                        <title>tag</title>
+                        <path class=" fill-amber-600"
+                            d="M5.5,7A1.5,1.5 0 0,1 4,5.5A1.5,1.5 0 0,1 5.5,4A1.5,1.5 0 0,1 7,5.5A1.5,1.5 0 0,1 5.5,7M21.41,11.58L12.41,2.58C12.05,2.22 11.55,2 11,2H4C2.89,2 2,2.89 2,4V11C2,11.55 2.22,12.05 2.59,12.41L11.58,21.41C11.95,21.77 12.45,22 13,22C13.55,22 14.05,21.77 14.41,21.41L21.41,14.41C21.78,14.05 22,13.55 22,13C22,12.44 21.77,11.94 21.41,11.58Z" />
+                    </svg>
+                </div>
+                <div class="grid grid-cols-[65%_35%] p-3">
+                    <h3 class="col-span-2 font-medium text-2xl">Nombre de la oferta</h3>
+                    <span class="text-sm text-[#1A6785] col-span-2">Nombre empresa</span>
+                    <p class="font-bold text-base">30 disponibles</p>
+                    <p class="font-bold text-[#919191] line-through place-self-end">$25.00</p>
+                    <span>15 días restantes</span>
+                    <p class="font-bold text-xl text-[#F97316] place-self-end">$20.00</p>
+                </div>
+            </div>
+            <div class="rounded-xl grid grid-cols-[auto_1fr] border-[#929191] border-[1px] overflow-hidden">
+                <div class="bg-[#a83131] w-[130px] flex justify-center items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-12" viewBox="0 0 24 24">
+                        <title>tag</title>
+                        <path class=" fill-amber-600"
+                            d="M5.5,7A1.5,1.5 0 0,1 4,5.5A1.5,1.5 0 0,1 5.5,4A1.5,1.5 0 0,1 7,5.5A1.5,1.5 0 0,1 5.5,7M21.41,11.58L12.41,2.58C12.05,2.22 11.55,2 11,2H4C2.89,2 2,2.89 2,4V11C2,11.55 2.22,12.05 2.59,12.41L11.58,21.41C11.95,21.77 12.45,22 13,22C13.55,22 14.05,21.77 14.41,21.41L21.41,14.41C21.78,14.05 22,13.55 22,13C22,12.44 21.77,11.94 21.41,11.58Z" />
+                    </svg>
+                </div>
+                <div class="grid grid-cols-[65%_35%] p-3">
+                    <h3 class="col-span-2 font-medium text-2xl">Nombre de la oferta</h3>
+                    <span class="text-sm text-[#1A6785] col-span-2">Nombre empresa</span>
+                    <p class="font-bold text-base">30 disponibles</p>
+                    <p class="font-bold text-[#919191] line-through place-self-end">$25.00</p>
+                    <span>15 días restantes</span>
+                    <p class="font-bold text-xl text-[#F97316] place-self-end">$20.00</p>
+                </div>
+            </div>
+            <div class="rounded-xl grid grid-cols-[auto_1fr] border-[#929191] border-[1px] overflow-hidden">
+                <div class="bg-[#a83131] w-[130px] flex justify-center items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-12" viewBox="0 0 24 24">
+                        <title>tag</title>
+                        <path class=" fill-amber-600"
+                            d="M5.5,7A1.5,1.5 0 0,1 4,5.5A1.5,1.5 0 0,1 5.5,4A1.5,1.5 0 0,1 7,5.5A1.5,1.5 0 0,1 5.5,7M21.41,11.58L12.41,2.58C12.05,2.22 11.55,2 11,2H4C2.89,2 2,2.89 2,4V11C2,11.55 2.22,12.05 2.59,12.41L11.58,21.41C11.95,21.77 12.45,22 13,22C13.55,22 14.05,21.77 14.41,21.41L21.41,14.41C21.78,14.05 22,13.55 22,13C22,12.44 21.77,11.94 21.41,11.58Z" />
+                    </svg>
+                </div>
+                <div class="grid grid-cols-[65%_35%] p-3">
+                    <h3 class="col-span-2 font-medium text-2xl">Nombre de la oferta</h3>
+                    <span class="text-sm text-[#1A6785] col-span-2">Nombre empresa</span>
+                    <p class="font-bold text-base">30 disponibles</p>
+                    <p class="font-bold text-[#919191] line-through place-self-end">$25.00</p>
+                    <span>15 días restantes</span>
+                    <p class="font-bold text-xl text-[#F97316] place-self-end">$20.00</p>
+                </div>
+            </div>
+            <div class="rounded-xl grid grid-cols-[auto_1fr] border-[#929191] border-[1px] overflow-hidden">
+                <div class="bg-[#a83131] w-[130px] flex justify-center items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-12" viewBox="0 0 24 24">
+                        <title>tag</title>
+                        <path class=" fill-amber-600"
+                            d="M5.5,7A1.5,1.5 0 0,1 4,5.5A1.5,1.5 0 0,1 5.5,4A1.5,1.5 0 0,1 7,5.5A1.5,1.5 0 0,1 5.5,7M21.41,11.58L12.41,2.58C12.05,2.22 11.55,2 11,2H4C2.89,2 2,2.89 2,4V11C2,11.55 2.22,12.05 2.59,12.41L11.58,21.41C11.95,21.77 12.45,22 13,22C13.55,22 14.05,21.77 14.41,21.41L21.41,14.41C21.78,14.05 22,13.55 22,13C22,12.44 21.77,11.94 21.41,11.58Z" />
+                    </svg>
+                </div>
+                <div class="grid grid-cols-[65%_35%] p-3">
+                    <h3 class="col-span-2 font-medium text-2xl">Nombre de la oferta</h3>
+                    <span class="text-sm text-[#1A6785] col-span-2">Nombre empresa</span>
+                    <p class="font-bold text-base">30 disponibles</p>
+                    <p class="font-bold text-[#919191] line-through place-self-end">$25.00</p>
+                    <span>15 días restantes</span>
+                    <p class="font-bold text-xl text-[#F97316] place-self-end">$20.00</p>
+                </div>
+            </div>
+            <div class="rounded-xl grid grid-cols-[auto_1fr] border-[#929191] border-[1px] overflow-hidden">
+                <div class="bg-[#a83131] w-[130px] flex justify-center items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="w-12" viewBox="0 0 24 24">
+                        <title>tag</title>
+                        <path class=" fill-amber-600"
+                            d="M5.5,7A1.5,1.5 0 0,1 4,5.5A1.5,1.5 0 0,1 5.5,4A1.5,1.5 0 0,1 7,5.5A1.5,1.5 0 0,1 5.5,7M21.41,11.58L12.41,2.58C12.05,2.22 11.55,2 11,2H4C2.89,2 2,2.89 2,4V11C2,11.55 2.22,12.05 2.59,12.41L11.58,21.41C11.95,21.77 12.45,22 13,22C13.55,22 14.05,21.77 14.41,21.41L21.41,14.41C21.78,14.05 22,13.55 22,13C22,12.44 21.77,11.94 21.41,11.58Z" />
+                    </svg>
+                </div>
+                <div class="grid grid-cols-[65%_35%] p-3">
+                    <h3 class="col-span-2 font-medium text-2xl">Nombre de la oferta</h3>
+                    <span class="text-sm text-[#1A6785] col-span-2">Nombre empresa</span>
+                    <p class="font-bold text-base">30 disponibles</p>
+                    <p class="font-bold text-[#919191] line-through place-self-end">$25.00</p>
+                    <span>15 días restantes</span>
+                    <p class="font-bold text-xl text-[#F97316] place-self-end">$20.00</p>
+                </div>
+            </div>
+        </div>
     </div>
     <style>
         .wallpaper {
