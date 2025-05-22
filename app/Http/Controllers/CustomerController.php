e6ee27b5e95eda283d6b86c5a9ec47150e78293a
10Oliver <oliver.erazo1@gmail.com>
2025-05-22 21:51:06 +0000

    ✨ Load coupons on landing

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,13 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Offer;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CustomerController extends Controller
 {
     public function index()
     {
-        return view('costumer.landing');
+        $company = Auth::user()->companies()->first();
+        $offers = Offer::where('state', '!=', false)
+        ->whereDate('end_date', '>=', now())
+        ->with('companies')
+        ->get();
+
+        return view('costumer.landing', compact('offers', 'company'));
     }
 
     public function cart()
