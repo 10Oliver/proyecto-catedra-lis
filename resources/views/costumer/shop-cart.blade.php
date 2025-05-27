c392134125a93d140d1fb8756d84335ba3801ca0
10Oliver <oliver.erazo1@gmail.com>
2025-05-27 14:55:44 +0000

    ♻️ Unify coupon cart in unique localstorage item

--- resources/views/costumer/shop-cart.blade.php
+++ resources/views/costumer/shop-cart.blade.php
@@ -1,16 +1,52 @@
 @extends('costumer.app')
 
 @section('content')
+    @php
+        $colors = ['bg-red-500', 'bg-blue-500', 'bg-green-500', 'bg-yellow-500', 'bg-purple-500'];
+        $icons = ['local_offer', 'star', 'shopping_cart', 'loyalty', 'emoji_events'];
+        /* $hash = crc32($offer->offer_uuid);
+        $color = $colors[$hash % count($colors)];
+        $icon = $icons[$hash % count($icons)]; */
+    @endphp
     <div class="w-full px-[7%] grid grid-cols-[70%_30%] my-10">
         <h1 class="col-span-2 text-center text-4xl font-bold mb-10">Carrito de compras</h1>
         <!-- #region Coupons list -->
-        <div class="flex flex-col gap-y-3">
-            <div class="w-[80%] border-[1px] border-black grid grid-cols-[30%_70%] bg-white">
+        <div class="flex flex-col gap-y-3" id="coupon-list">
+
+        </div>
+        <!-- #endregion -->
+        <div class="bg-[#E7E7E7] flex flex-col gap-y-5 p-5">
+            <h2 class="font-bold text-3xl">Resumen de pedido</h2>
+            <div class="flex bg-white justify-between py-3 px-5">
+                <span class="font-bold">Total</span>
+                <span>$40.00</span>
+            </div>
+            <div class="grid grid-cols-[70%_30%] bg-white p-5">
+                <span class="font-bold">Cantidad de productos</span>
+                <span class="place-self-end">1</span>
+                <span class="font-bold">Total ahorrado</span>
+                <span class="place-self-end">$15.00</span>
+            </div>
+            <div class="text-white bg-[#1A6785] py-2 text-center font-bold" id="pay-button">
+                Pagar
+            </div>
+        </div>
+    </div>
+
+    <script>
+        const payButton = document.getElementById("pay-button");
+
+        console.log(@json($offers))
+        payButton.addEventListener('click', () => {
+            location.href = @json(route('pay.view'));
+            console.log(@json(route('pay.view')));
+        });
+
+/*         <div class="w-[80%] border-[1px] border-black grid grid-cols-[30%_70%] bg-white">
                 <div class="flex justify-center items-center bg-amber-600">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="max-w-[50px]">
                         <title>star-four-points-outline</title>
-                        <path
-                            class=" fill-amber-300"
+                        <path class=" fill-amber-300"
                             d="M12,6.7L13.45,10.55L17.3,12L13.45,13.45L12,17.3L10.55,13.45L6.7,12L10.55,10.55L12,6.7M12,1L9,9L1,12L9,15L12,23L15,15L23,12L15,9L12,1Z" />
                     </svg>
                 </div>
@@ -45,37 +81,11 @@
                     <div class="place-self-end">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-7">
                             <title>delete</title>
-                            <path class="fill-[#E10000]" d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
+                            <path class="fill-[#E10000]"
+                                d="M19,4H15.5L14.5,3H9.5L8.5,4H5V6H19M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19Z" />
                         </svg>
                     </div>
                 </div>
-            </div>
-        </div>
-        <!-- #endregion -->
-        <div class="bg-[#E7E7E7] flex flex-col gap-y-5 p-5">
-            <h2 class="font-bold text-3xl">Resumen de pedido</h2>
-            <div class="flex bg-white justify-between py-3 px-5">
-                <span class="font-bold">Total</span>
-                <span>$40.00</span>
-            </div>
-            <div class="grid grid-cols-[70%_30%] bg-white p-5">
-                <span class="font-bold">Cantidad de productos</span>
-                <span class="place-self-end">1</span>
-                <span class="font-bold">Total ahorrado</span>
-                <span class="place-self-end">$15.00</span>
-            </div>
-            <div class="text-white bg-[#1A6785] py-2 text-center font-bold" id="pay-button">
-                Pagar
-            </div>
-        </div>
-    </div>
-
-    <script>
-        const payButton = document.getElementById("pay-button");
-
-        payButton.addEventListener('click', () => {
-            location.href = @json(route('pay.view'));
-            console.log(@json(route('pay.view')));
-        });
+            </div> */
     </script>
 @endsection
